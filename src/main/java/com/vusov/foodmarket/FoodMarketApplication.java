b752ecb9ebf74192725f1461649ba2617fcc6e25
Valya Usov <rossariokelly0671@gmail.com>
2022-07-27 17:15:00 +0300

    Initial commit

---
+++ src/main/java/com/vusov/foodmarket/FoodMarketApplication.java
@@ -0,0 +1,13 @@
+package com.vusov.foodmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodMarketApplication.class, args);
+    }
+
+}
